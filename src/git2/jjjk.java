d09960f3913056c254b4e295c9bea1757203705d
KamaryaEsrapil <138307647+KamaryaEsrapil@users.noreply.github.com>
2023-08-11 07:40:12 +0800

    variable and getters and setters were

--- src/git2/jjjk.java
+++ src/git2/jjjk.java
@@ -3,5 +3,6 @@ package git2;
 public class jjjk {
     public static void main(String[] args) {
         System.out.println("play flies");
+        int n = 9;
     }
 }
--- src/git2/kji.java
+++ src/git2/kji.java
@@ -1,9 +1,13 @@
 package git2;
 
 public class kji {
+    String make;
+    String model;
+    int year;
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+
 
         }
     }
